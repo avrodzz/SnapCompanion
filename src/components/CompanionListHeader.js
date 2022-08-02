03c8b0bf89d687d16c2c8b656cbeb8b7ddb843b3
Alexis Rodriguez <avrodzz@gmail.com>
2022-08-02 11:43:29 -0700

    Finished the CompanionListHeader component that should be in the scrollable list in the bottom sheet within CompanionProfileSheet.js

---
+++ src/components/CompanionListHeader.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { StyleSheet, View, Text, Button } from "react-native";
+import UserAvatar from 'react-native-user-avatar';
+
+export default function CompanionListHeader(props) {
+
+  return (
+    <View style={styles.container}>
+        <View style={styles.avatarContainer}>
+            <UserAvatar size={props.avatarSize} name={props.companionName}></UserAvatar>
+        </View>
+       
+        <View style={styles.headerTextContainer}>
+            <View style={styles.companionNameContainer}>
+                <Text style={styles.companionName}>{props.companionName}</Text>
+            </View>
+            <View style={styles.companionBirthdayContainer}>
+                <Text style={styles.companionBirthday}>Your companion since {props.companionBirthday}</Text>
+            </View>
+        </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    marginBottom: 15
+  },
+  avatarContainer: {
+    marginLeft: 10,
+    marginRight: 10
+  },
+  headerTextContainer: {
+
+  },
+  companionNameContainer: {
+    marginTop: 5,
+    marginBottom: 5
+  },
+  companionName: {
+    fontSize: 21.98
+  },
+  companionBirthdayContainer: {
+
+  },
+  companionBirthday: {
+
+  }
+});
