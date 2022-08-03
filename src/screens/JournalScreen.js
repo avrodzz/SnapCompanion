ba29225343562c64ce49048cafcd188fc429339c
Alexis Rodriguez <avrodzz@gmail.com>
2022-08-02 20:39:49 -0700

    Worked on the skeleton of the JournalScreen and created a TextInputComponent.

--- src/screens/JournalScreen.js
+++ src/screens/JournalScreen.js
@@ -1,24 +1,59 @@
-import React, { useCallback, useRef, useMemo } from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
-import BottomSheetComponent from "../components/BottomSheetComponent";
+import React, { useState } from "react";
+import { StyleSheet, View, Text, Button, TextInput } from "react-native";
+// import { TextArea, Box, Center, NativeBaseProvider, extendTheme } from "native-base";
 
-export default function JournalScreen({ navigation, route }) {
+export default function JournalScreen(props) {
+//   const [textAreaValue, setTextAreaValue] = useState("");
+  const [contentInput, onChangeContentInput] = useState("");
+
+  const demoValueControlledTextArea = e => {
+    setTextAreaValue(e.currentTarget.value);
+  };
+
+
   return(
-      <BottomSheetComponent />
+    <View style={styles.container}>
+        <View style={styles.titleTextContainer}>
+           <Text style={styles.titleText}>Title</Text> 
+        </View>
+        <View style={styles.contentInputContainer}>
+            <TextInput
+                style={styles.contentInput}
+                onChangeText={onChangeContentInput}
+                value={contentInput}
+                multiline
+            />
+        </View>
+        <View style={styles.saveButtonContainer}>
+            <Button title='Save' style={styles.saveButton}></Button>
+        </View>
+        
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 200,
+    flexDirection: 'column'
   },
-  contentContainer: {
-    backgroundColor: "white",
+  titleTextContainer: {
+    marginTop: 15,
+    marginBottom: 15,
   },
-  itemContainer: {
-    padding: 6,
-    margin: 6,
-    backgroundColor: "#eee",
+  titleText: {
+    textAlign: 'center',
+    fontSize: 30
   },
+  contentInputContainer: {
+    margin: 15,
+    backgroundColor: "#FFF",
+    height: 100
+  },
+  saveButtonContainer: {
+
+  },
+  saveButton: {
+
+  }
 });
