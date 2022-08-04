99afa8b8b782bfc59a2e1e3065c2ee670a638459
Alexis Rodriguez <avrodzz@gmail.com>
2022-08-04 01:53:02 -0700

    Redesigned the JournalScreen. The firebase connection still works, but the Saved Journals section has not yet been implemented.

--- src/screens/JournalScreen.js
+++ src/screens/JournalScreen.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
-import { SafeAreaView, StyleSheet, View, Text, Button, TextInput } from "react-native";
+import { SafeAreaView, StyleSheet, View, Text, Button, TextInput, Image, TouchableOpacity } from "react-native";
 import { doc, setDoc, updateDoc } from "firebase/firestore";
 import db from "../../firebase";
 import { useAuthentication } from "../utils/hooks/useAuthentication";
+import CameraButton from "../../assets/images/journalButtons/camera_button_yellow.png"
+import VoiceButton from "../../assets/images/journalButtons/voice_button_yellow.png"
+import JournalButton from "../../assets/images/journalButtons/journal_button_yellow.png"
 
 export default function JournalScreen({route}) {
 //   const [journalName, setJournalName] = useState("");
@@ -33,7 +36,9 @@ export default function JournalScreen({route}) {
   return(
     <SafeAreaView style={styles.container}>
         <View style={styles.titleTextContainer}>
-           <Text style={styles.titleText}>{journalName ? journalName : 'Title'}</Text> 
+           {/* <Text style={styles.titleText}>{journalName ? journalName : 'Title'}</Text>  */}
+           <Text style={styles.titleText}>Snap Journal</Text>
+           {/* <Text style={styles.journalNameText}>{journalName}</Text> */}
         </View>
         <View style={styles.contentInputContainer}>
             <TextInput
@@ -43,10 +48,26 @@ export default function JournalScreen({route}) {
                 multiline
             />
         </View>
-        <View style={styles.saveButtonContainer}>
-            <Button title='Save' style={styles.saveButton} onPress={onSave}></Button>
+        <View style={styles.journalButtonContainer}>
+            <View>
+                <TouchableOpacity onPress={() => console.log('voice journal button press')}>
+                    <Image source={VoiceButton} />
+                </TouchableOpacity>
+            </View>
+            <View>
+                <TouchableOpacity onPress={() => console.log('camera journal button press')}>
+                    <Image source={CameraButton} />
+                </TouchableOpacity>
+            </View>
+            <View>
+                <TouchableOpacity onPress={() => console.log('journal journal button press')}>
+                    <Image source={JournalButton} />
+                </TouchableOpacity>
+            </View>
         </View>
-        
+        <View style={styles.saveButtonContainer}>
+            <Button title='Save' style={styles.saveButton} onPress={onSave} />
+        </View>
     </SafeAreaView>
   )
 }
@@ -54,7 +75,8 @@ export default function JournalScreen({route}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column'
+    flexDirection: 'column',
+    backgroundColor: 'white'
   },
   titleTextContainer: {
     marginTop: 15,
@@ -65,17 +87,29 @@ const styles = StyleSheet.create({
     fontSize: 30
   },
   contentInputContainer: {
-    margin: 15,
+    marginTop: 15,
+    marginLeft: 15,
+    marginRight: 15,
     backgroundColor: "#FFF",
-    height: 100
+    height: 500,
+    borderWidth: 1,
+    borderRadius: 26.6456,
+    // justifyContent: 'center'
   },
   contentInput: {
-
+    padding: 10
+  },
+  journalButtonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
   },
   saveButtonContainer: {
-
+    marginBottom: 10,
+    color: 'black'
   },
   saveButton: {
-
+    color: '#000'
   }
 });
